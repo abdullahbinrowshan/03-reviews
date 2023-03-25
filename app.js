19ba5fcc8da01cfdf52a5f1ec341c09bc67e39df
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2023-03-25 09:10:45 +0600

    almost done except the random btn

--- app.js
+++ app.js
@@ -29,3 +29,45 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
+
+
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn')
+const nextBtn = document.querySelector('.next-btn')
+const randomBtn = document.querySelector('.random-btn');
+
+let currentItem = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson();
+})
+
+const showPerson = (person = currentItem ) => {
+  const item = reviews[person]
+  img.src = item.img
+  console.log(img);
+  console.log(item);
+  author.textContent = item.name
+  job.textContent = item.job
+  info.textContent = item.text
+}
+
+nextBtn.addEventListener('click', () => {
+  currentItem++
+  if (currentItem >= reviews.length ) {
+    currentItem = 0;
+  }
+  showPerson()
+})
+
+prevBtn.addEventListener('click', () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson()
+})
